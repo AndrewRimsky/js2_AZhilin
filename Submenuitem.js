6e52331af4f7c1f25b1a86fa3a1b3c08f332cd12
dsksandr <zhilin_aleksandr@live.ru>
2018-09-12 22:55:12 +0700

    Добавил домашнее задание к уроку 1

---
+++ Submenuitem.js
@@ -0,0 +1,12 @@
+'use strict';
+
+class SubMenuItem extends Menu{
+    constructor(id, className, items, href, title){
+        super(id, className, items);
+        this.href = href;
+        this.title = title;
+    }
+    render(){
+        return `<li><a href="${this.href}">${this.title}</a>${super.render()}</li>`
+    }
+}
